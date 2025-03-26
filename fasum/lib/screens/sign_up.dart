ba0b6c18ebb226fca8c8c2aba26ce8256d79f281
FelixHu03 <felix_2327250059@mhs.mdp.ac.id>
2025-03-26 19:56:57 +0700

    selesai membuat sign up screen

--- fasum/lib/screens/sign_up.dart
+++ fasum/lib/screens/sign_up.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/foundation.dart';
+import 'package:fasum/screens/sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SignUpScreen extends StatefulWidget {
@@ -9,10 +10,76 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
 
-  
+  void _registerAccount() async{
+    if (_passwordController.text != _confirmPasswordController.text) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: const Text("Password dan confirm password tidak sama, Silahkan Coba kembali!!"),
+        )
+      );
+      return;
+    }
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+      if (mounted) {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => const SignInScreen(),
+          )
+        );
+      }
+    } on FirebaseAuthException catch (e) {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Gagal mendaftar: ${e.message}'),
+          )
+        );
+      }
+    }
+   
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Registrasi Akun'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: "Email"),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(labelText: "Password"),
+            ),
+            TextField(
+              controller: _confirmPasswordController,
+              decoration: const InputDecoration(labelText: "Confirm Password"),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 16),
+              child: ElevatedButton(
+                onPressed: (){
+                  _registerAccount();
+                },
+                child: const Text("Daftar"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
