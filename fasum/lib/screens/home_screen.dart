7f0c29df6d4a65c5ddc97402f3a2621b888c53c0
FelixHu03 <felix_2327250059@mhs.mdp.ac.id>
2025-03-26 20:16:28 +0700

    sudah membuat home_screen dengan fitur logout

--- fasum/lib/screens/home_screen.dart
+++ fasum/lib/screens/home_screen.dart
@@ -1,16 +1,39 @@
-import 'package:flutter/foundation.dart';
+import 'package:fasum/screens/sign_in.dart';
+import 'package:fasum/screens/sign_up.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
+  Future<void> signOut(BuildContext context) async{
+    await FirebaseAuth.instance.signOut();
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (context) => const SignInScreen()
+      )
+    );
+  }
 
-class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        actions: [
+        IconButton(
+          onPressed: (){
+            signOut(context);
+          }, 
+          icon: const Icon(Icons.logout)
+          )
+        ],
+      ),
+      body: const Center(
+        child: const Text('You have logged in'),
+      ),
+    );
   }
 }
