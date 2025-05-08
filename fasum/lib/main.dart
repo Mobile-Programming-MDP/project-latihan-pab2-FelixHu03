6d659e256d1a8a146d6a0c9347f2af96f8d89482
FelixHu03 <felix_2327250059@mhs.mdp.ac.id>
2025-05-08 20:34:59 +0700

    mantap

--- fasum/lib/main.dart
+++ fasum/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:fasum/screens/home_screen.dart';
 import 'package:fasum/screens/sign_in.dart';
+import 'package:fasum/screens/splash_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -25,17 +26,18 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(), 
-        builder: (context, snapshot){
-          if (snapshot.hasData) {
-            return const HomeScreen();
-          }
-          else{
-            return const SignInScreen();
-          }
-        }
-        )
+      home: const SplashScreen(),
+      // home: StreamBuilder(
+      //   stream: FirebaseAuth.instance.authStateChanges(), 
+      //   builder: (context, snapshot){
+      //     if (snapshot.hasData) {
+      //       return const SplashScreen();
+      //     }
+      //     else{
+      //       return const SignInScreen();
+      //     }
+      //   }
+      //   )
     );
   }
 }
